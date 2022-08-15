b039b3c17232a42d84e807408e72c1715733973f
Song-Junho97 <devdream1117@gmail.com>
2022-08-15 16:11:54 +0900

    🗃️ TypeOrm으로 데이터베이스 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [UserModule, ConfigModule.forRoot({
     isGlobal: true,
-  })],
+  }),  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3307,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'super_db',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
